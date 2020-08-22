7652b8293b3684dc4dc99ea5fa3c8703763b3126
Harshit Shukla <53831906+zlatan007@users.noreply.github.com>
2020-08-22 13:14:16 +0530

    done

---
+++ dist/precache-manifest.04ce2ee55248de996c308f9913998725.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32d88440e33d292b39ad690ceb1c3782",
+    "url": "/index.html"
+  },
+  {
+    "revision": "953941f0af53c27a4b18",
+    "url": "/static/css/2.e962c2eb.chunk.css"
+  },
+  {
+    "revision": "4085fff9489c9e47afd9",
+    "url": "/static/css/main.1bc9ef74.chunk.css"
+  },
+  {
+    "revision": "953941f0af53c27a4b18",
+    "url": "/static/js/2.9b1435fd.chunk.js"
+  },
+  {
+    "revision": "7585d7e2191ead1c7fd01d7ebe90ade7",
+    "url": "/static/js/2.9b1435fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4085fff9489c9e47afd9",
+    "url": "/static/js/main.76902552.chunk.js"
+  },
+  {
+    "revision": "bd5e167a2a683df3ad47",
+    "url": "/static/js/runtime-main.dd2a4da3.js"
+  },
+  {
+    "revision": "f2425f17a305204ed707c1a4c455bd0e",
+    "url": "/static/media/call-to-action-paralax.f2425f17.jpg"
+  },
+  {
+    "revision": "35bed3b48acfa24008b0aa7b8b8c1050",
+    "url": "/static/media/cover-photo20.35bed3b4.jpg"
+  },
+  {
+    "revision": "c53d04ad4712f444f844713ca0be75a6",
+    "url": "/static/media/fb-subheader-2.c53d04ad.jpg"
+  },
+  {
+    "revision": "ac460e89f00b7d194d13c2844b01d8b0",
+    "url": "/static/media/fb-subheader-4.ac460e89.jpg"
+  },
+  {
+    "revision": "162ff4008c83359a06f3809c90223b52",
+    "url": "/static/media/home-top-1.162ff400.jpg"
+  },
+  {
+    "revision": "dab6f3cd7b3079a88ce8097368831415",
+    "url": "/static/media/how-to-work1.dab6f3cd.png"
+  },
+  {
+    "revision": "815ace79df7a4cc42f6ce3e2d4c4f016",
+    "url": "/static/media/how-to-work2.815ace79.png"
+  },
+  {
+    "revision": "02d846b8ff5c3ef15bce4f3f80b6b75e",
+    "url": "/static/media/how-to-work3.02d846b8.png"
+  },
+  {
+    "revision": "22be2bce134558546fd1288668ad01c6",
+    "url": "/static/media/listing-logo03.22be2bce.png"
+  },
+  {
+    "revision": "9debe726febc8dcaca36f38396081398",
+    "url": "/static/media/listing-logo09.9debe726.png"
+  },
+  {
+    "revision": "c9102623a160b0b860ebdaccb5846dec",
+    "url": "/static/media/listing-logo12.c9102623.png"
+  },
+  {
+    "revision": "1dd39ed1d99822bf21122e8ce6012a53",
+    "url": "/static/media/listing-logo13.1dd39ed1.png"
+  }
+]);
---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import MyRoutes from './config/routes'
+import { Provider } from 'react-redux';
+import store from './store'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css"
+import {
+  faStar, faHeart, faPhone, faEnvelope, faSearch, faUtensils,
+  faThumbsUp, faSortAlphaDown, faUserMinus, faDollarSign, faAngleDoubleRight, faPlus,
+  faConciergeBell, faCommentAlt, faInfoCircle, faShoppingBasket, faTimes, faSpinner, faTruck, faTasks,
+} from '@fortawesome/free-solid-svg-icons';
+
+library.add(faStar, faHeart, faPhone, faEnvelope, faSearch, faUtensils,
+  faThumbsUp, faSortAlphaDown, faUserMinus, faDollarSign, faAngleDoubleRight, faPlus,
+  faConciergeBell, faCommentAlt, faInfoCircle, faShoppingBasket, faTimes, faSpinner, faTruck, faTasks,)
+
+class App extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <div>
+          <MyRoutes />
+        </div>
+      </Provider>
+    );
+  }
+}
+
+export default App;
